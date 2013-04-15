7570e7f5c46a6daf68e70738a91b3e2fabb00cfb
Sean Billig <sean.billig@gmail.com>
2013-04-15 09:19:57 -0500

    Constants

--- gorient.go
+++ gorient.go
@@ -7,11 +7,67 @@ import (
 )
 
 type Command byte
+
 const (
-	SHUTDOWN Command = iota + 1
-	CONNECT
-	DB_OPEN
-	DB_CREATE
+	SHUTDOWN                       Command = 1
+	CONNECT                                = 2
+	DB_OPEN                                = 3
+	DB_CREATE                              = 4
+	DB_CLOSE                               = 5
+	DB_EXIST                               = 6
+	DB_DROP                                = 7
+	DB_SIZE                                = 8
+	DB_COUNTRECORDS                        = 9
+	DATACLUSTER_ADD                        = 10
+	DATACLUSTER_DROP                       = 11
+	DATACLUSTER_COUNT                      = 12
+	DATACLUSTER_DATARANGE                  = 13
+	DATACLUSTER_COPY                       = 14
+	DATACLUSTER_LH_CLUSTER_IS_USED         = 16
+	DATASEGMENT_ADD                        = 20
+	DATASEGMENT_DROP                       = 21
+	RECORD_METADATA                        = 29
+	RECORD_LOAD                            = 30
+	RECORD_CREATE                          = 31
+	RECORD_UPDATE                          = 32
+	RECORD_DELETE                          = 33
+	RECORD_COPY                            = 34
+	RECORD_CHANGE_IDENTITY                 = 35
+	POSITIONS_HIGHER                       = 36
+	POSITIONS_LOWER                        = 37
+	RECORD_CLEAN_OUT                       = 38
+	POSITIONS_FLOOR                        = 39
+	COUNT                                  = 40 // REQUEST_DATACLUSTER_COUNT
+	COMMAND                                = 41
+	POSITIONS_CEILING                      = 42
+	TX_COMMIT                              = 60
+	CONFIG_GET                             = 70
+	CONFIG_SET                             = 71
+	CONFIG_LIST                            = 72
+	DB_RELOAD                              = 73
+	DB_LIST                                = 74
+	PUSH_RECORD                            = 79
+	PUSH_DISTRIB_CONFIG                    = 80
+
+	// DISTRIBUTED
+	DB_COPY     = 90
+	REPLICATION = 91
+	CLUSTER     = 92
+	DB_TRANSFER = 93
+
+	// Lock + sync
+	DB_FREEZE  = 94
+	DB_RELEASE = 95
+
+	// INCOMING
+	STATUS_OK    = 0
+	STATUS_ERROR = 1
+	PUSH_DATA    = 3
+
+	// CONSTANTS
+	RECORD_NULL              int16 = -2
+	RECORD_RID               int16 = -3
+	CURRENT_PROTOCOL_VERSION int16 = 15
 )
 
 type Xx struct {
